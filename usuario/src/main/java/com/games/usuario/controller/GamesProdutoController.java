1b79794d1566058b31ffb7a4d7ad063d6e42ca3a
Raphael Scaziti <rscaziti@gmail.com>
2021-02-25 17:20:37 -0300

    Adicionado método PUT na controller de produto

--- usuario/src/main/java/com/games/usuario/controller/GamesProdutoController.java
+++ usuario/src/main/java/com/games/usuario/controller/GamesProdutoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,4 +50,9 @@ public class GamesProdutoController
 		return ResponseEntity.status(HttpStatus.CREATED).body(this.repository.save(produto));
 	}
 	
+	@PutMapping
+	public ResponseEntity<GamesProdutosModel> put(@RequestBody GamesProdutosModel produto)
+	{
+		return ResponseEntity.status(HttpStatus.OK).body(this.repository.save(produto));
+	}
 }
