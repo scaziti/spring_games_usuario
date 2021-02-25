eadf12b27d6a695cb756bffeec549f6cb92b84fd
Raphael Scaziti <rscaziti@gmail.com>
2021-02-25 14:27:08 -0300

    Adicionado método POST na controller de usuário

--- usuario/src/main/java/com/games/usuario/controller/GamesUsuarioController.java
+++ usuario/src/main/java/com/games/usuario/controller/GamesUsuarioController.java
@@ -3,10 +3,13 @@ package com.games.usuario.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.games.usuario.model.GamesUsuarioModel;
@@ -38,5 +41,11 @@ public class GamesUsuarioController
 	public ResponseEntity<List<GamesUsuarioModel>> GetByNome(@PathVariable String nomeUsuario)
 	{
 		return ResponseEntity.ok(this.repository.findAllByNomeUsuarioContainingIgnoreCase(nomeUsuario));
+	}
+	
+	@PostMapping
+	public ResponseEntity<GamesUsuarioModel> post(@RequestBody GamesUsuarioModel usuario)
+	{
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.repository.save(usuario));
 	}
 }
