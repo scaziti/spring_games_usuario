164fa3d1b5cfef2b4d7a225f21ca34650288bb58
Raphael Scaziti <rscaziti@gmail.com>
2021-02-25 14:31:11 -0300

    Adicionado método PUT na controller de usuário

--- usuario/src/main/java/com/games/usuario/controller/GamesUsuarioController.java
+++ usuario/src/main/java/com/games/usuario/controller/GamesUsuarioController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,5 +48,11 @@ public class GamesUsuarioController
 	public ResponseEntity<GamesUsuarioModel> post(@RequestBody GamesUsuarioModel usuario)
 	{
 		return ResponseEntity.status(HttpStatus.CREATED).body(this.repository.save(usuario));
+	}
+	
+	@PutMapping
+	public ResponseEntity<GamesUsuarioModel> put(@RequestBody GamesUsuarioModel usuario)
+	{
+		return ResponseEntity.status(HttpStatus.OK).body(this.repository.save(usuario));
 	}
 }
