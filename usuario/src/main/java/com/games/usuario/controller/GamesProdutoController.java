f006ad97310f68688dee3d54020cd3da763ac75c
Raphael Scaziti <rscaziti@gmail.com>
2021-02-25 15:57:46 -0300

    Adicionado método POST na controller de produto

--- usuario/src/main/java/com/games/usuario/controller/GamesProdutoController.java
+++ usuario/src/main/java/com/games/usuario/controller/GamesProdutoController.java
@@ -2,10 +2,13 @@ package com.games.usuario.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,6 +41,12 @@ public class GamesProdutoController
 	public ResponseEntity<List<GamesProdutosModel>> GetByDescricao(@PathVariable String descricaoProduto)
 	{
 		return ResponseEntity.ok(this.repository.findAllByDescricaoProdutoContainingIgnoreCase(descricaoProduto));
+	}
+	
+	@PostMapping
+	public ResponseEntity<GamesProdutosModel> post(@RequestBody GamesProdutosModel produto)
+	{
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.repository.save(produto));
 	}
 	
 }
