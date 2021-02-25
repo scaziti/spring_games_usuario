a4f0e6e5a088830d6930e4e99916073cd60634b4
Raphael Scaziti <rscaziti@gmail.com>
2021-02-25 15:03:14 -0300

    Adicionado método POST na controller de categoria

--- usuario/src/main/java/com/games/usuario/controller/GamesCategoriaController.java
+++ usuario/src/main/java/com/games/usuario/controller/GamesCategoriaController.java
@@ -3,10 +3,13 @@ package com.games.usuario.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.games.usuario.model.GamesCategoriaModel;
@@ -32,5 +35,17 @@ public class GamesCategoriaController
 		return this.repository.findById(idCategoria)
 				.map(resp -> ResponseEntity.ok(resp))
 				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/nomecategoria/{nomeCategoria}")
+	public ResponseEntity<List<GamesCategoriaModel>> GetByNome(@PathVariable String nomeCategoria)
+	{
+		return ResponseEntity.ok(this.repository.findAllByNomeCategoriaContainingIgnoreCase(nomeCategoria));
+	}
+	
+	@PostMapping
+	public ResponseEntity<GamesCategoriaModel> post(@RequestBody GamesCategoriaModel categoria)
+	{
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.repository.save(categoria));
 	}
 }
