2ea6ecba7f9908be604ff657a5c700def60f27d5
Raphael Scaziti <rscaziti@gmail.com>
2021-02-25 14:57:06 -0300

    Adicionado método GetById na controller de categoria

--- usuario/src/main/java/com/games/usuario/controller/GamesCategoriaController.java
+++ usuario/src/main/java/com/games/usuario/controller/GamesCategoriaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.games.usuario.model.GamesCategoriaModel;
@@ -23,5 +24,13 @@ public class GamesCategoriaController
 	public ResponseEntity<List<GamesCategoriaModel>> GetAll()
 	{
 		return ResponseEntity.ok(this.repository.findAll());
+	}
+	
+	@GetMapping("/{idCategoria}")
+	public ResponseEntity<GamesCategoriaModel> GetById(@PathVariable long idCategoria)
+	{
+		return this.repository.findById(idCategoria)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
 	}
 }
