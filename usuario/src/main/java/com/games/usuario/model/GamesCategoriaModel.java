011f74f574ed00226f208f1a7acd06023f7a7570
Raphael Scaziti <rscaziti@gmail.com>
2021-02-25 14:52:56 -0300

    Adicionado Model de categoria

---
+++ usuario/src/main/java/com/games/usuario/model/GamesCategoriaModel.java
@@ -0,0 +1,51 @@
+package com.games.usuario.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+public class GamesCategoriaModel 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idCategoria;
+	
+	@NotNull
+	@Size(min = 2, max = 150)
+	private String nomeCategoria;
+	
+	@NotNull
+	private Boolean ativo;
+
+	public Long getIdCategoria() {
+		return idCategoria;
+	}
+
+	public void setIdCategoria(Long idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+
+	public String getNomeCategoria() {
+		return nomeCategoria;
+	}
+
+	public void setNomeCategoria(String nomeCategoria) {
+		this.nomeCategoria = nomeCategoria;
+	}
+
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+	
+	
+}
