d768382b6096d9064158f438fbbf395b11fdcdff
Raphael Scaziti <rscaziti@gmail.com>
2021-02-26 09:33:26 -0300

    Refactor(repository/categoria/usuario): acrescentado anotação @Repository nas interfaces de repository

--- usuario/src/main/java/com/games/usuario/repository/GamesCategoriaRepository.java
+++ usuario/src/main/java/com/games/usuario/repository/GamesCategoriaRepository.java
@@ -3,8 +3,11 @@ package com.games.usuario.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.games.usuario.model.GamesCategoriaModel;
 
+@Repository
 public interface GamesCategoriaRepository extends JpaRepository<GamesCategoriaModel, Long>
 {
 	public List<GamesCategoriaModel> findAllByNomeCategoriaContainingIgnoreCase(String nomeCategoria);
--- usuario/src/main/java/com/games/usuario/repository/GamesUsuarioRepository.java
+++ usuario/src/main/java/com/games/usuario/repository/GamesUsuarioRepository.java
@@ -2,9 +2,11 @@ package com.games.usuario.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.games.usuario.model.GamesUsuarioModel;
 
+@Repository
 public interface GamesUsuarioRepository extends JpaRepository<GamesUsuarioModel, Long>
 {
 	public List<GamesUsuarioModel> findAllByNomeUsuarioContainingIgnoreCase(String nomeUsuario);
